75d0ef3e1277377fc7445637558bf68f5f2629ab
Benjamin Rietman <benjaminrietman@Benjamins-MacBook-Pro.local>
2020-06-17 23:18:41 -0400

    finished welcome screen; worked on navigation bar

---
+++ script/main.js
@@ -0,0 +1,8 @@
+// Navigation for mobile
+function mainNavFunction() {
+    document.getElementById("my-navigation").classList.toggle("show-main-nav");
+}
+
+function subNavFunction() {
+    document.getElementById("my-sub-navigation").classList.toggle("show-sub-nav");
+}
